6e73da98b41359316609add580056348730f49c1
Murielle Lethrosne <murielle.lethrosne@developpement-durable.gouv.fr>
2020-04-15 11:43:25 +0200

    Création du squelette du package

---
+++ devstuff_history.R
@@ -0,0 +1,17 @@
+#permet d'ignorer le fichier, ainsi pas de note lors du check
+usethis::use_build_ignore("devstuff_history.R")
+
+# Définir une licence
+usethis::use_gpl3_license("GPL-3")
+
+# package utile
+#usethis::use_package("nomdupackage")
+
+# Crée un fichier .R dans le dossier R
+#usethis::use_r("nomdemafonction")
+
+
+#creation de la documentation des data
+#usethis::use_r("doc_nomdeladata")
+
+
