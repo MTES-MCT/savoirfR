3d08141207e2b06b5b3c4f83016a3374f7fee211
Murielle Lethrosne <murielle.lethrosne@developpement-durable.gouv.fr>
2020-04-23 08:38:18 +0200

    Ajout exo2 - ajout ODD_graphique1 - modif list_data_module.csv #7

---
+++ inst/rstudio/templates/project/ressources/m5/enonces/exo2.R
@@ -0,0 +1,12 @@
+# A partir des données de la table ODD_graphique1, reprendre le nuage de point obtenu à partir des données ODD :
+# taux de mortalité maternelle (Maternal_mortality_ratio) 
+# en fonction du produit intérieur brut (Gross_Domestic_Product_GDP) (Chapitre 4.1)
+# 
+# En modifier le thème:
+#   
+# -  Insérer un titre de couleur, en gras, centré par rapport au graphe.
+# -  Indiquer la source (ODD) et modifier sa taille
+# -  Changer les titres des axes et leurs tailles.
+# -  modifier la couleur de fond du graphe
+# -  modifier le type de gadrillage (pointillés)
+
