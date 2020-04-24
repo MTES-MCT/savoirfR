faf42b600336185f7f0db9f06f29facce52a8a1d
Murielle Lethrosne <murielle.lethrosne@developpement-durable.gouv.fr>
2020-04-24 11:20:29 +0200

    Ajout exo 7 hchart #7

---
+++ inst/rstudio/templates/project/ressources/m5/enonces/exo7.R
@@ -0,0 +1,4 @@
+# A partir du fichier rpls_aggrege_large, réaliser un graphique en barre représentant 
+# la répartition des DPE par classe (en %).
+# Le graphique sera animé par département, uniquement pour ceux de la région Centre-Val de Loire
+
