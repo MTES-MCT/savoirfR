f6e8df2ea65e34aad4b87c415dc87b0bf442b938
Murielle Lethrosne <murielle.lethrosne@developpement-durable.gouv.fr>
2020-04-27 11:47:08 +0200

    ajout vignette M5  - modif devstuff_history  - ajout exercices_module_datavi...Rmd #7

--- inst/rstudio/templates/project/ressources/m5/enonces/exo1.R
+++ inst/rstudio/templates/project/ressources/m5/enonces/exo1.R
@@ -1,6 +1,6 @@
 # premier exercice
 
-# A partir des données de la table rpls_aggrege_large (dans le data_exercices_formation.RData),
+# A partir des données de la table rpls_aggrege_large (RData),
 # créer un nuage de point entre:
 #     - le % de parc social de moins de 5 ans;(variable Parc_de_moins_de_5_ans_pourcent)
 #     - et le pourcentage de logement avec un DPE ges de classe A,B ou C;(variable DPE_GES_classe_ABC_pourcent)
