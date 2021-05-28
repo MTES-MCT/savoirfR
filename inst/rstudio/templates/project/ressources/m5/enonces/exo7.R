d5918e4b97686532407fa5132fe3e93c33aa77b6
Murielle Lethrosne <murielle.lethrosne@developpement-durable.gouv.fr>
2021-05-28 08:30:36 +0200

    Modif exo 7 pour utilisation de ggiraph au lieu de highcharter. Monter version 0.0.0.9000 à 0.0.0.9001

--- inst/rstudio/templates/project/ressources/m5/enonces/exo7.R
+++ inst/rstudio/templates/project/ressources/m5/enonces/exo7.R
@@ -1,7 +1,6 @@
-# Reprendre le nuage de point obtenu à partir des données ODD :
-# taux de mortalité maternelle (Maternal_mortality_ratio) 
-# en fonction du produit intérieur brut (Gross_Domestic_Product_GDP) (Chapitre 4.1)
-
+# Reprendre le nuage de point obtenu à partir des données ODD :taux de mortalité maternelle 
+# (Maternal_mortality_ratio) en fonction du produit intérieur brut (Gross_Domestic_Product_GDP) (Chapitre 9)
+# 
 # Obtenir ce graphique pour le web avec des infos bulles regroupant le nom du continent, 
-# de la zone et les données associées
-
+# de la zone, le PIB et le taux de mortalité maternelle.
+# Paramètrer la surbrillance du continent au survol d'élément.
