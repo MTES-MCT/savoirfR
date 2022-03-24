f0aab55dabad47130b237bc64657180c434d9a03
jdlom <jdaniel.lomenede@gmail.com>
2022-03-24 15:14:02 +0100

    better regex to match rmdfile

--- dev/extract_r_files.R
+++ dev/extract_r_files.R
@@ -2,7 +2,9 @@
 # et Extrait les enonces et les corrections des rmd et les place dans les dossiers
 #
 
-files <- list.files(file.path("inst", "vignettes"), recursive = TRUE)
+files <- list.files(file.path("inst", "vignettes"), 
+                    recursive = TRUE, 
+                    pattern = "[Rr](md|markdown)$")
 lapply(files, FUN = function(.file) {
   module <- fs::path_dir(.file)
   rmdfile <- fs::path_file(.file)
