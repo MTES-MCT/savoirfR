1c64727e12b782c93ffd262ca7216e023adc3a68
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2023-12-08 19:16:35 +0100

    Un peu d'aide pour le module 4

--- inst/rstudio/templates/project/ressources/m5/corrections/exo4.R
+++ inst/rstudio/templates/project/ressources/m5/corrections/exo4.R
@@ -1,9 +1,9 @@
 # ---
 # title: "Exercice 4 -  module 5"
 # ---  
-# Réaliser une facette sur un diagramme barre avec :       
-#  - en variable discrète les régions,    
-#  - en variables continues les indicateurs suivants :      
+# A partir de la table rpls_aggrege, réaliser une facette sur un diagramme barre avec :       
+#  - en variable discrète les régions (axe des x),    
+#  - en variables continues les indicateurs suivants (axe des y, les facettes) :      
 #     - % de logements de 3 et 4 pièces   
 #     - % DPE énergie A, B, C   
 #     - % DPE GES A, B, C   
