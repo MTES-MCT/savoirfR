ef17a0be8205d1d6ded94764e12ac531e6112fe0
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2023-12-08 19:03:48 +0100

    M5 - exo 3, changement de la palette demandée

--- inst/rstudio/templates/project/ressources/m5/enonces/exo3.R
+++ inst/rstudio/templates/project/ressources/m5/enonces/exo3.R
@@ -2,7 +2,7 @@
 # title: "Exercice 3 -  module 5"
 # --- 
 # En réutilisant le graphe obtenu à l’exercice 1 (rpls_aggrege_large.RData), y rajouter :
-#  - une palette brewer pour la couleur ;
+#  - une palette gouvdown pour la couleur ;
 #  - la légende en bas ;
 #  - des libellés (axes et légende) parlant et un titre.
 
