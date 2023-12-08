8c09f173d09b5428647a6fe6af84516f3057450e
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2023-12-08 19:41:33 +0100

    Ajout de précision dans la consigne de l'exo 5 du M5

--- inst/rstudio/templates/project/ressources/m5/corrections/exo5.R
+++ inst/rstudio/templates/project/ressources/m5/corrections/exo5.R
@@ -1,7 +1,7 @@
 # ---
 # title: "Exercice 5 -  module 5"
 # ---   
-# Produire une carte du Loiret à l'EPCI représentant le taux de logements collectifs dans le parc locatif social (variable "Logements collectifs_pourcent").   
+# A partir des jeux de données rpls_aggrege.RData et Carte_EPCI_France.RData, produire une carte du Loiret à l'EPCI représentant le taux de logements collectifs dans le parc locatif social (variable "Logements collectifs_pourcent").   
 load("extdata/rpls_aggrege.RData")
 load("extdata/Carte_EPCI_France.RData")
 
