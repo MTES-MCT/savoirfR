7e36402814221e3ed8d9d80632a6df857ff79462
juliette.engelaere <juliette.engelaere@developpement-durable.gouv.fr>
2026-05-22 16:56:35 +0200

    - ajout vignette pkgdown M6 - ajout enonces M6 dans dossier stagiaire - fix pkg check note "Found the following non-portable file paths:     - savoirfR/inst/rstudio/templates/project/ressources/m6/corrections/cheesedown_exo7bis_exemple_interactivite_bis.Rmd     - savoirfR/inst/rstudio/templates/project/ressources/m6/corrections/img/Quels-sont-les-fromages-a-deguster-en-automne.png     - savoirfR/inst/rstudio/templates/project/ressources/m6/corrections/img/Quels-sont-les-fromages-a-deguster-en-automne.webp

---
+++ inst/rstudio/templates/project/ressources/m6/enonces/exo1.R
@@ -0,0 +1,9 @@
+# ---
+# title: "Exercice 1 - module 6"
+# ---  
+# -   Ouvrir Rstudio
+# -   Créer un fichier Rmarkdown, format html
+# -   prévisualiser le document avant toutes modifications
+# -   Se mettre en auteur du document
+# -   Rajouter un theme spécifique
+# -   Cliquer sur *knit* pour compiler le document et identifier les correspondances entre fichier .Rmd et fichier html
---
+++ inst/rstudio/templates/project/ressources/m6/enonces/exo5.R
@@ -0,0 +1,16 @@
+# ---
+# title: "Exercice 5 -  module 6"
+# ---  
+# ## Exercice 5
+#   - Créer un nouveau .Rmd
+#   - Ajouter comme paramètre dans le YAML le pays (country) en choisissant "France" comme valeur par défaut
+#   - Ajouter un titre pour qu'il dépende de ce paramètre
+#   - Filtrer la dataframe en fonction de ce paramètre
+#   - Ajouter un graphique construit à partir de cette dataframe filtrée. Son titre peut également dépendre du paramètre country. (voir la [page dédiée à la réalisation de graphique avec ggplot dans le module 5 du parcours R](https://mtes-mct.github.io/parcours_r_module_datavisualisation/package-ggplot2.html), ou réutiliser le code ci-dessous) 
+# Par exemple, on peut regrouper les fromages par type simplifié et représenter le pourcentage de matières grasses par type.
+# C'est l'occasion de découvrir ou re-découvrir certaines nouvelles fonctions de dplyr apparues en 2026 :
+
+#   - Générer ce HTML à l'aide du bouton knit
+#   - Créer dans un nouveau script une fonction qui génère le HTML à partir d'une liste de pays
+#   - Appliquer cette fonction à une liste restreinte de pays (par exemple France, Italy, United States)
+#   
