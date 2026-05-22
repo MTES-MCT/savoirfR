d66699dd3173fda1fa32ae499fd9bacb11ea7e61
juliette.engelaere <juliette.engelaere@developpement-durable.gouv.fr>
2026-05-22 16:04:00 +0200

    Adaptation des fonctions savoirFr aux spécificités du M6 - Pas de propagation rmd_to pour les corrections - skeleton (création du projet stagaire) :    * 1/on renomme le fichier de correction de l'exercice 1 (qui s'appelle index.html pour être rendu comme un site web)    * 2/ on ne cree pas de dossier /exdata pour rien

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,23 +1,16 @@
 #' get files from the exercices package
-#
 #' @param ... other params from the systeme.file fonction
-#'
 #' @import stats utils
 #' @keywords internal
-
 pkg_file <- function(...) {
   system.file(..., package = 'savoirfR', mustWork = TRUE)
 }
 
-
 #' get extdata file needed for a module
-#'
 #' @param m module number
 #' @importFrom utils read.csv2
 #' @return a list of files
 #' @keywords internal
-
-
 module_data <- function(m) {
   # if (m=='m1'){
   #   files <- list("Base_synth_territoires.csv","rp_2012.csv")
@@ -32,16 +25,15 @@ module_data <- function(m) {
 #' Function to define the project exo_modules
 #'
 #' @param path path where the project is create
-#' @param ...  params define by the user a the project creation
-#'
+#' @param ...  params define by the user a the project creation, dans notre cas module suivi par le stagiaire 
+#' (cf liste des parametres possible dans le fichier. dcf de `inst/rstudio/templates/project`)
 #' @return create a project
 #' @keywords internal
-
+#' 
 exo_modules_skeleton = function(path, ...) {
 
   # ensure directory exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
-  dir.create(file.path(path, 'extdata'), recursive = TRUE, showWarnings = FALSE)
   dir.create(file.path(path, 'scripts'), recursive = TRUE, showWarnings = FALSE)
   dir.create(file.path(path, 'corrections'), recursive = TRUE, showWarnings = FALSE)
 
@@ -50,34 +42,33 @@ exo_modules_skeleton = function(path, ...) {
   dots <- list(...)
   m <- dots[[1]]
 
-  # copy 'extdata' folder to path
+  # copy 'extdata' folder to path if needed
   files = module_data(m)
-  extdata <- pkg_file('extdata')
-  source <- file.path(extdata, files)
-  target = file.path(path, 'extdata')
-  
-  file.copy(source, target, recursive = TRUE)
+  if(length(files) > 0) {
+    dir.create(file.path(path, 'extdata'), recursive = TRUE, showWarnings = FALSE)
+    extdata <- pkg_file('extdata')
+    source <- file.path(extdata, files)
+    target = file.path(path, 'extdata')    
+    file.copy(source, target, recursive = TRUE) 
+  }
 
   # copy 'enonces' folder
-
   ressources = pkg_file('rstudio', 'templates', 'project', 'ressources', m, 'enonces')
-
   files = list.files(ressources, recursive = TRUE, include.dirs = FALSE)
-
   source = file.path(ressources, files)
   target = file.path(path, 'scripts', files)
   file.copy(source, target)
 
   # copy 'corrections' folder
-
   ressources = pkg_file('rstudio', 'templates', 'project', 'ressources', m, 'corrections')
-
   files = list.files(ressources, recursive = TRUE, include.dirs = FALSE)
-
   source = file.path(ressources, files)
   target = file.path(path, 'corrections', files)
   file.copy(source, target)
-
-
+  
+  # M6 corrections are special, need to be rendered as a website
+  # so we need to rename file of ex 1 from index.Rmd to cheesedown_exo1.Rmd
+  file.rename(from = file.path(path, 'corrections', "index.Rmd"), 
+              to =   file.path(path, 'corrections', "cheesedown_exo1.Rmd"))
   TRUE
 }
