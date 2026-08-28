01f9b0535ab31578eaaa9b4722544dbbd962c1f0
juliette.engelaere <juliette.engelaere@developpement-durable.gouv.fr>
2026-08-28 15:29:29 +0200

    - Prise en compte des remarques de Christelle Belkacem (suppression de magick dans exo3, regroupement des packages à charger en début d'exercice) cf https://github.com/MTES-MCT/parcours_r_module_publication_rmarkdown/issues/69 - Ajustements suite intégration du jeu de données dans savoirfR et test contexte stagiaire (recours à here) - Correction de coquilles dans les énoncés - Ajout des fichiers multimedia des dossiers img/ et www/ de cheesdown dans le projet du stagiaire

--- R/skeleton.R
+++ R/skeleton.R
@@ -12,9 +12,6 @@ pkg_file <- function(...) {
 #' @return a list of files
 #' @keywords internal
 module_data <- function(m) {
-  # if (m=='m1'){
-  #   files <- list("Base_synth_territoires.csv","rp_2012.csv")
-  # }
   list_data <- file.path(pkg_file('extdata'), 'list_data_module.csv')
   data <- read.csv2(list_data, colClasses = c("character"))
   files <- as.list(data[data$module == m, ]$file)
@@ -68,9 +65,16 @@ exo_modules_skeleton = function(path, ...) {
   
   # M6 corrections are special, need to be rendered as a website
   # so we need to rename ex1 file from index.Rmd to cheesedown_exo1.Rmd
+  # and to get extra files as images, css...
   if(m == 'm6') {
     file.rename(from = file.path(path, 'corrections', "index.Rmd"), 
-                to =   file.path(path, 'corrections', "cheesedown_exo1.Rmd"))    
+                to =   file.path(path, 'corrections', "cheesedown_exo1.Rmd"))  
+    extra_files_source = list.files(paste(ressources, c("img", "www"), sep = "/"), recursive = TRUE, full.names = TRUE) 
+    extra_files_dest = file.path(path, gsub(".*corrections/", "", extra_files_source))
+    dir.create(file.path(path, "img"))
+    dir.create(file.path(path, "www"))
+    file.copy(extra_files_source, extra_files_dest, recursive = TRUE)
+    unlink(file.path(path, 'corrections', "_site.yml"))
   }
 
   
